a4acebee20aa9bcb35a676de9f7479660d324054
jei623 <jedisom@gmail.com>
2016-04-18 15:26:19 -0400

    feat: Create mean days since character seen feature

--- Chinese_Learning/feature_creation.py
+++ Chinese_Learning/feature_creation.py
@@ -27,34 +27,41 @@ def char_counts(df):
     corpus = df.loc[:,'text_read']
     dtm = vectorizer.fit_transform(corpus)
 
-    #Get percent of characters not seen in text so far
-    import numpy as np    
-    #from scipy.sparse import csr_matrix
+    
+    import numpy as np  
+    from itertools import chain
+    import datetime
     
     n = df.shape[0]
     df.loc[:,'percent_seen'] = 0.0
     for i in range(1, n):   #cycle through all rows except first row
-        prior_non_zero = dtm[:i,:].nonzero()[1]    #Find non-zero values in sparse matrix in (i-1) records
-        before_chars = np.unique(prior_non_zero)  #Get list of all characters that have been seen so far
-        current_chars = np.sort(dtm[i,:].nonzero()[1]) #Find non-zero characters in current record as column #'s
-        
-        #Use characters that match previous non-zero characters and current characters to calculate % seen        
+        #Get percent of characters not seen in text so far        
+        prior_non_zero = dtm[:i,:].nonzero()    #Find non-zero values in sparse matrix in (i-1) records
+        before_chars = np.unique(prior_non_zero[1])  #Get list of all characters that have been seen so far
+        current_chars = np.sort(dtm[i,:].nonzero()[1]) #Find non-zero characters in current record as column #'s    
         #http://stackoverflow.com/questions/28901311/numpy-find-index-of-elements-in-one-array-that-occur-in-another-array
         matching_current_index = np.where(np.in1d(current_chars, before_chars))[0]
         df.loc[i,'percent_seen'] = float(matching_current_index.shape[0])/float(current_chars.shape[0])
         
-        #matching_chars = current_chars[matching_current_index]       
-        #for j in list(matching_current_index):
-            #http://stackoverflow.com/questions/10252766/python-numpy-get-array-locations-of-a-list-of-values
-            #[np.where(prior_non_zero[1]==k) for k in list(matching_current_index)]
-        
-    #Get mean days since characters last read (for those already seen in text)
-        
+        #Get mean days since characters last read (for those already seen in text)        
+        #http://stackoverflow.com/questions/10252766/python-numpy-get-array-locations-of-a-list-of-values
+        #http://stackoverflow.com/questions/11860476/how-to-unnest-a-nested-list         
+        #Gets list of tuple arrays (1 array per char in matching chars) where each array gives the indices of 
+        #prior_non_zero where that character can be found        
+        matching_chars = current_chars[matching_current_index]
+        prior_array_indices = [np.where(prior_non_zero[1] == k) for k in list(matching_chars)]
+        prior_array_indices = list(chain(*prior_array_indices))
+        last_date_indices = map(lambda x: max(x), prior_array_indices)        
+        last_date_rows = prior_non_zero[0][last_date_indices]
+        current_date = df.loc[i,'date']
+        days_since_seen = map(lambda x: current_date - x, df.loc[last_date_rows, 'date'])
+        df.loc[i,'mean_days_since_seen'] = (sum(days_since_seen, datetime.timedelta(0)).total_seconds()
+                                            / 86400.0 / (len(days_since_seen)))
 
     return df    
     
 def create_features(df):
     
     new_df = Add_cumsums(df)
-    #new_df = char_counts(new_df)
+    new_df = char_counts(new_df)
     return new_df
